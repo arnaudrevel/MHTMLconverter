e284414994eba083d15aea6a2671c90449eb6855
Arnaud Revel <revel.arnaud@gmail.com>
2023-04-30 18:06:40 +0200

    Add requirements in setup.cfg

--- setup.py
+++ setup.py
@@ -1,2 +1,11 @@
 from setuptools import setup
-setup()
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name = "MHTMLconverter",
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
