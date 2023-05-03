08600cdf2d22f79e63ddbe4cd652e4f90cd26927
Arnaud Revel <revel.arnaud@gmail.com>
2023-05-03 12:40:27 +0200

    Add a test for css files

--- tests/test_html.py
+++ tests/test_html.py
@@ -29,3 +29,15 @@ def test_local_html() -> None:
     mhtmlconverter.mhtml.url_to_mhtml("./tests/resources/test_local.html","./tests/resources/test_local.mhtml")
 
     assert(out.exists())
+
+def test_css_html() -> None:
+    """
+        Test HTML
+    """
+    out = pathlib.Path("./tests/resources/test_css.mhtml")
+
+    if out.exists():    out.unlink()
+
+    mhtmlconverter.mhtml.url_to_mhtml("https://github.com/arnaudrevel/MHTMLconverter","./tests/resources/test_css.mhtml")
+
+    assert(out.exists())    
