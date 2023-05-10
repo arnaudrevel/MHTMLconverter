558ca7cd2410009dcb88f1f085e836243af154dc
Arnaud Revel <revel.arnaud@gmail.com>
2023-05-10 22:23:06 +0200

    Add complex tests for mhtml2html

--- tests/test_mhtml.py
+++ tests/test_mhtml.py
@@ -23,3 +23,33 @@ def test_mhtml() -> None:
     mhtmlconverter.mhtml.mhtml_to_html("./tests/resources/test_mhtml.mhtml","results/test_mhtml.html")
 
     assert(out.exists())
+
+def test_mhtml_complex() -> None:
+    """
+        Test MHTML
+    """
+    out = pathlib.Path("./tests/resources/results/complex/test_mhtml.html")
+    outdir = pathlib.Path("./tests/resources/results/complex/_resources")
+    
+    if out.exists():    out.unlink()
+    if outdir.exists():
+        shutil.rmtree(outdir.resolve())
+    
+    mhtmlconverter.mhtml.mhtml_to_html("./tests/resources/test_mhtml.mhtml","results/complex/test_mhtml.html")
+
+    assert(out.exists())
+
+def test_mhtml_relative() -> None:
+    """
+        Test MHTML
+    """
+    out = pathlib.Path("./tests/resources/results/test_relative/test_mhtml.html")
+    outdir = pathlib.Path("./tests/resources/results/relative_resources")
+    
+    if out.exists():    out.unlink()
+    if outdir.exists():
+        shutil.rmtree(outdir.resolve())
+    
+    mhtmlconverter.mhtml.mhtml_to_html("./tests/resources/test_mhtml.mhtml","results/test_relative/test_mhtml.html","../relative_resources")
+
+    assert(out.exists())
