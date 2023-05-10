3bf22b89297b4f803786a63df639e15e5f74158f
Arnaud Revel <revel.arnaud@gmail.com>
2023-05-10 17:25:38 +0200

    Modify test_mhtml to echo other modifications

--- tests/test_fileutility.py
+++ tests/test_fileutility.py
@@ -14,19 +14,19 @@ def test_rewrite_if_relative() -> None:
 
     # Test relative file with no absolute path > returns absolute path
     assert(mhtmlconverter.fileutility.to_absolute("test_mhtml.mhtml")
-        ==(pathlib.Path(".")/"test_mhtml.mhtml").resolve())
+        ==str((pathlib.Path(".")/"test_mhtml.mhtml").resolve()))
 
     # Test relative relative dir with relative path > returns path relative to relative dir
     assert(mhtmlconverter.fileutility.to_absolute("_resources","test_mhtml.mhtml")
-        ==(pathlib.Path(".")/"_resources").resolve())
+        ==str((pathlib.Path(".")/"_resources").resolve()))
 
     # Test relative path with absolute path > returns path relative to absolute path
     assert(mhtmlconverter.fileutility.to_absolute("_resources",pathlib.Path("test.mhtml").resolve())
-        ==(pathlib.Path(".")/"_resources").resolve())
+        ==str((pathlib.Path(".")/"_resources").resolve()))
 
     # Test absolute path with absolute path > stays the path unchanged
     assert(mhtmlconverter.fileutility.to_absolute(pathlib.Path("../_resources").resolve(),
-        pathlib.Path("test.mhtml").resolve())==pathlib.Path("../_resources").resolve())
+        pathlib.Path("test.mhtml").resolve())==str(pathlib.Path("../_resources").resolve()))
 
 def test_get_res_path() -> None:
     """
