2c4b61d2277030a38293991bd8bfbd0fb23956a3
Arnaud Revel <revel.arnaud@gmail.com>
2023-05-10 17:54:15 +0200

    Add all config flags to mhtml2html

--- mhtmlconverter/cli/mhtml2html.py
+++ mhtmlconverter/cli/mhtml2html.py
@@ -12,12 +12,13 @@ logging.basicConfig(level=logging.INFO,format="\x1b[32;20m %(filename)s > \x1b[3
 @click.command(help="MHTML to HTML converter")
 @click.option("-i","--input","mhtmlfile",required=True, help="mhtml input filename")
 @click.option("-o","--output","outputfile",default="output.html",help="html output filename")
-def main(htmlfile:str = "index.mhtml",outputfile:str = "output.html")-> None:
+@click.option("-r","--resources","resourcesdir",default="_resources",help="resources directory")
+def main(mhtmlfile:str = "index.mhtml",outputfile:str = "output.html", resourcesdir:str = "_resources")-> None:
     """
     Main program
     Converts the 'mhtmlfile' into an html file
     """
-    mhtml.mhtml_to_html(htmlfile,outputfile)
+    mhtml.mhtml_to_html(mhtmlfile,outputfile,resourcesdir)
 
  # Main entry point
 if __name__ == "__main__":
