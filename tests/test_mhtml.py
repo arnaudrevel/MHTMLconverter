ed09398be9d7147d1acd17fab6f81fd47bb874c5
Arnaud Revel <revel.arnaud@gmail.com>
2023-05-11 14:50:31 +0200

    Add assertion to check path exists

--- tests/test_mhtml.py
+++ tests/test_mhtml.py
@@ -35,8 +35,8 @@ def test_mhtml_complex() -> None:
     if outdir.exists():
         shutil.rmtree(outdir.resolve())
     
+    assert(pathlib.Path("./tests/resources/results/complex/").exists())
     mhtmlconverter.mhtml.mhtml_to_html("./tests/resources/test_mhtml.mhtml","results/complex/test_mhtml.html")
-
     assert(out.exists())
 
 def test_mhtml_relative() -> None:
@@ -49,9 +49,10 @@ def test_mhtml_relative() -> None:
     if out.exists():    out.unlink()
     if outdir.exists():
         shutil.rmtree(outdir.resolve())
-    
+ 
     mhtmlconverter.mhtml.mhtml_to_html("./tests/resources/test_mhtml.mhtml","results/test_relative/test_mhtml.html","../relative_resources")
 
+    assert(pathlib.Path("./tests/resources/results/test_relative/").exists())
     assert(out.exists())
 
 def test_mhtml_with_css():
